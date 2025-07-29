c33f69652e9571e405af643b0bf6c8a59920d8fe
JxSH-07 <jashdjsce36@gmail.com>
2025-07-29 18:36:09 +0530

    Cleaned Up file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.jsx";
-import NotFound from "./pages/NotFound.jsx";
 import EditProfile from "./components/EditProfile.jsx";
-
-const queryClient = new QueryClient();
+import NotFound from "./pages/NotFound.jsx";
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/Edit" element={<EditProfile />}></Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/Edit" element={<EditProfile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
 );
 
 export default App;
